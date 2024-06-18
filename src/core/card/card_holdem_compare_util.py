1a12e13def9f3f39d4d95ec4c2cc36453e6b2b34
Guancheng Li <guanchenglichina@qq.com>
2024-06-18 20:06:38 +0800

    [core]Add utils and uts for 5 cards comparison

--- src/core/card/card_holdem_compare_util.py
+++ src/core/card/card_holdem_compare_util.py
@@ -12,60 +12,106 @@ from core.card.card_holdem_type import (
 
 
 def compare_2_group(group_1: List[Card], group_2: List[Card]) -> int:
-    group_1_value = card_type_of_5_cards(group_1)
-    group_2_value = card_type_of_5_cards(group_2)
-    if group_1_value > group_2_value:
-        return 1
-    elif group_1_value < group_2_value:
-        return -1
-    return compare_2_group_in_same_type(group_1, group_2, group_1_value)
+    group_1_type = card_type_of_5_cards(group_1)
+    group_2_type = card_type_of_5_cards(group_2)
+    if group_1_type != group_2_type:
+        return compare_2_diff_type(group_1_type, group_2_type)
+    return compare_2_group_in_same_type(group_1, group_2, group_1_type.value)
+
+
+def compare_2_diff_type(type_1: CardType, type_2: CardType):
+    assert type_1 != type_2
+    return 1 if type_1.value > type_2.value else -1
 
 
 def compare_2_group_in_same_type(
-    group_1: List[Card], group_2: List[Card], card_type: CardType) -> int:
-    group_1_sorted = sort_card_by_point(group_1)
-    group_1_grouped = group_by_point(group_1_sorted)
-    group_2_sorted = sort_card_by_point(group_2)
-    group_2_grouped = group_by_point(group_2_sorted)
+    cards_1: List[Card], cards_2: List[Card], card_type: CardType) -> int:
+    cards_1_sorted = sort_card_by_point(cards_1)
+    cards_1_grouped = group_by_point(cards_1_sorted)
+    cards_2_sorted = sort_card_by_point(cards_2)
+    cards_2_grouped = group_by_point(cards_2_sorted)
     if card_type == CardType.ROYAL_FLUSH:
         return 0  # Only 1 royal flush could exist.
     elif card_type == CardType.STRAIGHT_FLUSH:
-        return _compare_straight_flush(group_1_sorted, group_2_sorted)
+        # All colors are equal.
+        return _compare_straight(cards_1_sorted, cards_2_sorted)
     elif card_type == CardType.FOUR_OF_A_KIND:
-        return _compare_four_of_a_kind(group_1_grouped, group_2_grouped)
+        return _compare_with_priority(cards_1_grouped, cards_2_grouped, 4)
+    elif card_type == CardType.FULL_HOUSE:
+        return _compare_with_priority(cards_1_grouped, cards_2_grouped, 3)
+    elif card_type == CardType.FLUSH:
+        return _compare_reversed_points(cards_1_sorted, cards_2_sorted)
+    elif card_type == CardType.STRAIGHT:
+        return _compare_straight(cards_1_sorted, cards_2_sorted)
+    elif card_type == CardType.THREE_OF_A_KIND:
+        return _compare_with_priority(cards_1_grouped, cards_2_grouped, 3)
+    elif card_type == CardType.TWO_PAIR:
+        return _compare_with_priority(cards_1_grouped, cards_2_grouped, 2)
+    elif card_type == CardType.ONE_PAIR:
+        return _compare_with_priority(cards_1_grouped, cards_2_grouped, 2)
+    elif card_type == CardType.HIGH_CARD:
+        return _compare_reversed_points(cards_1_sorted, cards_2_sorted)
+    return 0  # Impossible to reach here.
 
+
+def _compare_straight(cards_1_sorted: List[Card], cards_2_sorted: List[Card]) -> int:
+    if cards_1_sorted[0].point() != 1 and cards_2_sorted[0].point() != 1:
+        if cards_1_sorted[0].point() > cards_2_sorted[0].point():
+            return 1
+        elif cards_1_sorted[0].point() < cards_2_sorted[0].point():
+            return -1
+        return 0
+    if cards_1_sorted[0].point() == 1 and cards_2_sorted[0].point() != 1:
+        if cards_1_sorted[1].point() == 2:
+            return -1
+        else:
+            return 1
+    if cards_1_sorted[0].point() != 1 and cards_2_sorted[0].point() == 1:
+        if cards_2_sorted[1].point() == 2:
+            return 1
+        else:
+            return -1
+    if cards_1_sorted[1].point() == cards_2_sorted[1].point():
+        return 0
+    return 1 if cards_1_sorted[1].point() > cards_2_sorted[1].point() else -1
+
+
+def _separate_high_low_factor(group: Dict[int, int], target_count) -> Tuple[List[int], List[int]]:
+    high = []
+    low = []
+    for key, value in group.items():
+        if value == target_count:
+            high.append(key)
+        else:
+            low.append(key)
+    assert len(high) > 0
+    assert len(low) > 0
+    return sorted(high, reverse=True), sorted(low, reverse=True)
+
+
+def _compare_sorted_list(points_1: List[int], points_2: List[int]) -> int:
+    assert len(points_1) == len(points_2)
+    for i in range(len(points_1)):
+        if points_1[i] > points_2[i]:
+            return 1
+        elif points_1[i] < points_2[i]:
+            return -1
     return 0
 
 
-def _compare_straight_flush(group_1: List[Card], group_2: List[Card]) -> int:
-    if group_1[0].point() > group_2[0].point():
-        return 1
-    elif group_1[0].point() < group_2[0].point():
-        return -1
-    return 0
+def _compare_with_priority(
+    cards_1_sorted: List[int], cards_2_sorted: List[int], factor_cnt: int) -> int:
+    high_1, low_1 = _separate_high_low_factor(cards_1_sorted, factor_cnt)
+    high_2, low_2 = _separate_high_low_factor(cards_2_sorted, factor_cnt)
+    high_res = _compare_sorted_list(high_1, high_2)
+    if high_res != 0:
+        return high_res
+    return _compare_sorted_list(low_1, low_2)
 
 
-def _compare_four_of_a_kind(group_1: Dict[int, int], group_2: Dict[int, int]) -> int:
-    def _get_duplicated_and_single(group: Dict[int, int]) -> Tuple[int, int]:
-        duplicated = None
-        single = None
-        for key, value in group.items:
-            if value == 4:
-                duplicated = key
-            else:
-                single == key
-        return duplicated, single
-    group_1_duplicated, group_1_single = _get_duplicated_and_single(group_1)
-    group_2_duplicated, group_2_single = _get_duplicated_and_single(group_2)
-    if group_1_duplicated > group_2_duplicated:
-        return 1
-    elif group_1_duplicated < group_2_duplicated:
-        return -1
-
-    if group_1_single > group_2_single:
-        return 1
-    elif group_1_single < group_2_single:
-        return -1
-    return 0
-
-
+def _compare_reversed_points(cards_1_sorted: List[Card], cards_2_sorted: List[Card]) -> int:
+    points_1 = [card.point() for card in cards_1_sorted]
+    points_1.reverse()
+    points_2 = [card.point() for card in cards_2_sorted]
+    points_2.reverse()
+    return _compare_sorted_list(points_1, points_2)
